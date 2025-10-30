dd255f7df79ac7ce95d7a78a8537be5c1ce1d731
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-10-31 04:54:36 +0500

    Task3

---
+++ Tyuiu.IvanovSV.Sprint3.Task3.V29/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.IvanovSV.Sprint3.Task3.V29.Lib;
+namespace Tyuiu.IvanovSV.Sprint3.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+            Console.WriteLine("****************************************************************************");
+
+            string value = "chgr vhhtg hnht";
+            char item = 'h';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Удаляемый символ = " + item);
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Преобразованная строка = " + ds.DeleteCharInString(value, item));
+            Console.ReadKey();
+
+        }
+    }
+}
