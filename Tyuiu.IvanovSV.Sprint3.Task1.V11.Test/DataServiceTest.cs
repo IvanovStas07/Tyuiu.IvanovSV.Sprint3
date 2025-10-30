27e6291762101d05893ffbbe3f78f50817123f39
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-10-31 04:34:40 +0500

    Task1

--- Tyuiu.IvanovSV.Sprint3.Task1.V11.Test/DataServiceTest.cs
+++ Tyuiu.IvanovSV.Sprint3.Task1.V11.Test/DataServiceTest.cs
@@ -1,10 +1,13 @@
 ﻿using Tyuiu.IvanovSV.Sprint3.Task1.V11.Lib;
 namespace Tyuiu.IvanovSV.Sprint3.Task1.V11.Test
 {
+    [TestClass]
     public class DataServiceTest
     {
+        [TestMethod]
         public void ValidGetMUltSeries()
         {
+            
             DataService ds = new DataService();
 
             double value = 4;
@@ -13,7 +16,7 @@ namespace Tyuiu.IvanovSV.Sprint3.Task1.V11.Test
 
             double res = ds.GetMultiplySeries(value, startValue, endValue);
 
-            double wait = 0;
+            double wait = 1064.25;
 
             Assert.AreEqual(wait, res);
         }
