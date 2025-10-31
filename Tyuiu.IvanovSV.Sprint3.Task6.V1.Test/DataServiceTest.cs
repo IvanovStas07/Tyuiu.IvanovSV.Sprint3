18249587896fffb87fcae13453ea3f774a62d521
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-10-31 05:51:10 +0500

    Task6

---
+++ Tyuiu.IvanovSV.Sprint3.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.IvanovSV.Sprint3.Task6.V1.Lib;
+
+namespace Tyuiu.IvanovSV.Sprint3.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 151;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
